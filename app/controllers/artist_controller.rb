1ab80dfb930794839c97cca8dcba550ec54d796b
Jeffe <Jeffe.bernal@gmail.com>
2016-10-31 15:21:25 -0500

    Artist and albums show full

---
+++ app/controllers/artist_controller.rb
@@ -0,0 +1,10 @@
+class ArtistController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @albums = @artist.albums
+  end
+end
